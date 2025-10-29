8c84798ce297dedbdef2d7503fd5779863e5b5e3
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-10-29 14:55:43 +0530

    set theme according to localstorage saved theme

--- JS/setDarkOrLight/script.js
+++ JS/setDarkOrLight/script.js
@@ -9,19 +9,32 @@ function setDarkORLight(){
     }
 }
 
-setDarkORLight();
 
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function(){
-    setDarkORLight();
+if (localStorage.getItem('theme')){
+    document.body.classList.add(localStorage.getItem('theme'))
+}
+else{
+    setDarkORLight()
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function()
+{   if(!localStorage.getItem("theme")){
+       setDarkORLight();
+}
 })
 
 let a= document.getElementById('theme-toggle')
 a.addEventListener("click",function(){
     if(document.body.classList.contains('dark-theme')){
         document.body.className = 'light-theme'
+
+        localStorage.setItem("theme", "light-theme")
     }
     else{
         document.body.className = 'dark-theme'
+
+        localStorage.setItem("theme", "dark-theme")
     }
 
 })
+
