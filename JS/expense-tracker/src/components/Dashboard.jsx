6476ad4ad17a51045b58ad03d8adbf79a386d267
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2026-01-02 14:59:26 +0530

    Edit functionality issue fix

--- JS/expense-tracker/src/components/Dashboard.jsx
+++ JS/expense-tracker/src/components/Dashboard.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ExpenseContext } from '../context/ExpenseContext'
 
 const Dashboard = () => {
-    const {transactions} = useContext(ExpenseContext);
+    const {transactions, deleteTransaction, updateTransaction} = useContext(ExpenseContext);
 
     const totalIncome = transactions
         .filter(t => t.type === 'income')
@@ -16,6 +16,10 @@ const Dashboard = () => {
         .reduce((acc, t) => acc + t.amount, 0);
 
     const totalBalance = totalIncome - totalExpenses;
+
+    const [editingId, setEditingId] =useState(null);
+    const [editAmount, setEditAmount] = useState(0);
+    const [editedDescription, setEditedDescription] = useState('');
 
   return (
     <div className="w-full h-full flex flex-col justify-between p-5">
@@ -38,6 +42,78 @@ const Dashboard = () => {
                 </button>
             </Link>
         </div>
+
+        <div className="mt-4">
+            <h2 className="text-xl font-bold mb-2">All Transactions</h2>
+            {/* If no transactions */}
+            {transactions.length === 0 && ( <p className='text-gray-500'>No transactions yet</p>)}
+
+            {/* List of transactions */}
+            <ul className='space-y-3'>
+                {transactions.map((t) => (
+                    <li key={t.id} className="flex justify-between items-center bg-white p-3 rounded shadow">
+            {/* EDIT MODE */}
+            {editingId === t.id ? (
+            <div className="flex items-center gap-2 w-full">
+    
+            {/* Edit Description */}
+            <input type="text" value={editedDescription} 
+                onChange={(e) => setEditedDescription(e.target.value)} 
+                className="border px-2 py-1 rounded w-48" placeholder="Description" 
+            />
+
+            {/* Edit Amount */}
+            <input type="number" value={editAmount}
+                onChange={(e) => setEditAmount(e.target.value)}
+                className="border px-2 py-1 rounded w-32"
+            />
+
+            {/* Save Button */}
+            <button onClick={() => {updateTransaction({ ...t,description: editedDescription.trim(), amount: Number(editAmount),}); 
+                setEditingId(null); setEditedDescription(''); setEditAmount(0);}}
+                className="bg-green-500 text-white px-3 py-1 rounded">
+                Save
+            </button>
+
+            {/* Cancel Button */}
+            <button onClick={() => setEditingId(null)} className="bg-gray-400 text-white px-3 py-1 rounded" > Cancel </button>
+        </div>
+            ) : (
+
+              /* VIEW MODE */
+              <>
+                <span>
+                  <strong>{t.description}</strong> — ₹{t.amount} (
+                  <span className={t.type === "income" ? "text-green-600": "text-red-600"}>
+                    {t.type}
+                  </span>
+                  )
+                </span>
+
+                <div className="flex gap-2">
+                  <button onClick={() => { setEditingId(t.id); setEditAmount(t.amount); setEditedDescription(t.description);}}
+                    className="bg-blue-500 text-white px-3 py-1 rounded"
+                  >
+                    Edit
+                  </button>
+
+                  <button
+                    onClick={() =>{ const confirmDelete = window.confirm("Are yu sure you want to delete this transaction?");
+                    if (confirmDelete) {
+                        deleteTransaction(t.id);
+                        }}
+                    }
+                    className="bg-red-500 text-white px-3 py-1 rounded">
+                        Delete
+                  </button>
+                </div>
+              </>
+            )}
+          </li>
+        ))}
+        </ul>
+        </div>
+
     </div>
   )
 }
--- JS/expense-tracker/src/context/ExpenseContext.jsx
+++ JS/expense-tracker/src/context/ExpenseContext.jsx
@@ -1,17 +1,32 @@
 import { createContext } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const ExpenseContext = createContext();
 
 export const ExpenseProvider =({children})=>{
-    const [transactions, setTransactions] = useState([]);
+    const [transactions, setTransactions] = useState(() => {
+        const stored = localStorage.getItem("transactions");
+        return stored ? JSON.parse(stored) : [];
+    });
 
     const addTransaction = (transaction) => {
-        setTransactions([...transactions, transaction]);
+        setTransactions([...transactions, {...transaction, id: Date.now() + Math.random(),}]);
     }
 
+    const deleteTransaction = (id) => {
+        setTransactions(transactions.filter(t => t.id !== id));
+    }
+
+    const updateTransaction = (updatedTransaction) => {
+        setTransactions(prevTransactions => prevTransactions.map(t => t.id === updatedTransaction.id ? updatedTransaction : t));
+    }
+
+    useEffect(() => {
+        localStorage.setItem("transactions", JSON.stringify(transactions));
+    },[transactions]);
+
     return(
-        <ExpenseContext.Provider value={{transactions, addTransaction}}>
+        <ExpenseContext.Provider value={{transactions, addTransaction, deleteTransaction, updateTransaction}}>
             {children}
         </ExpenseContext.Provider>
     )
